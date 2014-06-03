f162a219c62528256bf045638039d13ec88fc6ff
David Gustafson <git@radicalskeptic.net>
2014-06-03 11:15:57 -0700

    modifies user factory to use faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,16 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
+  sequence(:email) { |n| "#{Faker::Name.first_name}#{n}@#{Faker::Internet.domain_name}" }
+  sequence(:name) { |n| "#{Faker::Name.first_name}#{n}" }
+
   factory :user do
+    name
+    email
+    password { Faker::Lorem.characters(10) }
+    password_confirmation { |user| user.password }
+    after(:build) { |user| user.skip_confirmation! }
   end
 end
