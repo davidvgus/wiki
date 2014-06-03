17cd60753f29400cf9b474b89f3d95c5097755b9
David Gustafson <git@radicalskeptic.net>
2014-06-03 00:00:10 -0700

    copied sendgrid setup to environment file for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,16 @@ Wiki::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'lit-beyond-3700.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '587',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => 'heroku.com'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
