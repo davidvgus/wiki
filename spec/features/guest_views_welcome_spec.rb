9b72af8287c4b2e144a3d0a3f27597e1aa57ec46
David Gustafson <git@radicalskeptic.net>
2014-06-05 21:55:39 -0700

    Adds feature test to show that guests do not see private wikis

--- spec/features/guest_views_welcome_spec.rb
+++ spec/features/guest_views_welcome_spec.rb
@@ -16,6 +16,30 @@ feature 'Guest views welcome page' do
     expect(page).to have_content('Lions')
     expect(page).to have_content('Tigers')
     expect(page).to have_content('Bears')
-
   end
+
+  feature 'Guest doesn see private wikis' do
+    scenario 'public wikis are visible' do
+      %w{Lions Tigers Bears}.each do |page|
+        create(:page, title: page)
+      end
+
+      %w{LaserSharks PolarWeasels}.each do |private_pages|
+        create(:page, title: private_pages, private: true)
+      end
+
+      visit root_path
+
+      expect(page).to have_content('guest account')
+      expect(page).to have_content('Sign In')
+      expect(page).to have_content('Sign Up')
+      expect(page).to have_content('Lions')
+      expect(page).to have_content('Tigers')
+      expect(page).to have_content('Bears')
+
+      expect(page).not_to have_content('LaserSharks')
+      expect(page).not_to have_content('PolarWeasels')
+    end
+  end
+
 end
