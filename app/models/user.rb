9d0a14928572576a382c231d81dbb95fb52b3af4
David Gustafson <git@radicalskeptic.net>
2014-06-06 11:27:56 -0700

    Adds joint_pages relation to user to find collaboration papers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   belongs_to :account
   has_many :pages, foreign_key: "owner_id"
+  has_many :collaborations
+  has_many :joint_pages, through: :collaborations, source: :page
   #validates_presence_of :account
 
   before_create :build_account
