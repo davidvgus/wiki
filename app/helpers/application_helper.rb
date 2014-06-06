2fba157e9f49f513aea52fd4dad2b12351d1f063
David Gustafson <git@radicalskeptic.net>
2014-06-05 19:31:32 -0700

    Creates markdown helper and runs wiki page content through it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def markdown(text)
+    extensions = { no_intra_emphasis: true, autolink: true }
+    renderer = Redcarpet::Render::HTML.new()
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
+  end
 end
