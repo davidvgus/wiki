9020bfc5a8e757b892998fead5a75cdb3cbcdee3
David Gustafson <git@radicalskeptic.net>
2014-06-07 12:27:52 -0700

    Move epic editor functionality and importing back to application.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,30 +1,31 @@
 
-var opts = {
-  container: 'epiceditor',
-  textarea: 'my-edit-area',
-  basePath: '/epiceditor',
-  parser: marked,
-  theme: {
-    base: '/themes/base/epiceditor.css',
-    preview: '/themes/preview/preview-dark.css',
-    editor: '/themes/editor/epic-dark.css'
-  },
-  button: {
-    preview: true,
-    bar: "auto"
-  },
-  focusOnLoad: false,
-  shortcut: {
-    modifier: 18,
-    fullscreen: 70,
-    preview: 80
-  },
-  string: {
-    togglePreview: 'Toggle Preview Mode',
-    toggleEdit: 'Toggle Edit Mode',
-    toggleFullscreen: 'Enter Fullscreen'
-  },
-  autogrow: true
-};
-var editor = new EpicEditor(opts).load();
-$("#my-edit-area").hide();
+//var opts = {
+  //container: 'epiceditor',
+  //textarea: 'my-edit-area',
+  //basePath: '/epiceditor',
+  //parser: marked,
+  //theme: {
+    //base: '/themes/base/epiceditor.css',
+    //preview: '/themes/preview/preview-dark.css',
+    //editor: '/themes/editor/epic-dark.css'
+  //},
+  //button: {
+    //preview: true,
+    //bar: "auto"
+  //},
+  //focusOnLoad: false,
+  //shortcut: {
+    //modifier: 18,
+    //fullscreen: 70,
+    //preview: 80
+  //},
+  //string: {
+    //togglePreview: 'Toggle Preview Mode',
+    //toggleEdit: 'Toggle Edit Mode',
+    //toggleFullscreen: 'Enter Fullscreen'
+  //},
+  //autogrow: true
+//};
+//$("#my-edit-area").hide();
+//var editor = new EpicEditor(opts).load();
+////$("#my-edit-area").hide();
